e318e289d7a9fcf8791567f83364080e83c3f758
Dylan C <dylanjohncolligan@gmail.com>
2024-12-10 13:09:54 +0000

    add intro

--- BattleShips/BattleShips/obj/Debug/net8.0/BattleShips.AssemblyInfo.cs
+++ BattleShips/BattleShips/obj/Debug/net8.0/BattleShips.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleShips")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92033590a444da301416c1f48e5fb43e535b3fd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bd79a9268e113b4cc8d3a389ac43c55a5094d88")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleShips")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleShips")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
