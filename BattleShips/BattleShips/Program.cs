9b16f138c3b44d167a62b6b136c4907c7df79e69
Dylan C <57530020+DJCrules@users.noreply.github.com>
2024-12-12 11:10:00 +0000

    Bettter looking

--- BattleShips/BattleShips/Program.cs
+++ BattleShips/BattleShips/Program.cs
@@ -1,12 +1,47 @@
-﻿static string[] intro()
+﻿using System;
+using System.Buffers;
+
+namespace BattleShips
 {
-    Console.WriteLine("Welcome to BattleShips\n");
-    Console.Write("Enter First Name: ");
-    player1 = Console.ReadLine();
-    Console.Write("\nEnter Second Name: ");
-    player2 = Console.ReadLine();
-    return [player1, player2];
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            intro();
+        }
+        //Main Procedures
+        static string[] intro()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(" ______  ______  ______ ______ __      ______    \r\n/\\  == \\/\\  __ \\/\\__  _/\\__  _/\\ \\    /\\  ___\\   \r\n\\ \\  __<\\ \\  __ \\/_/\\ \\\\/_/\\ \\\\ \\ \\___\\ \\  __\\   \r\n \\ \\_____\\ \\_\\ \\_\\ \\ \\_\\  \\ \\_\\\\ \\_____\\ \\_____\\ \r\n  \\/_____/\\/_/\\/_/  \\/_/   \\/_/ \\/_____/\\/_____/ \r\n                                                 \r\n       ______  __  __  __  ______ ______         \r\n      /\\  ___\\/\\ \\_\\ \\/\\ \\/\\  == /\\  ___\\        \r\n      \\ \\___  \\ \\  __ \\ \\ \\ \\  _-\\ \\___  \\       \r\n       \\/\\_____\\ \\_\\ \\_\\ \\_\\ \\_\\  \\/\\_____\\      \r\n        \\/_____/\\/_/\\/_/\\/_/\\/_/   \\/_____/  \n\n", Console.ForegroundColor);
+            loadbar(50);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\n\nEnter First Player: ", Console.ForegroundColor);
+            string? player1 = Console.ReadLine();
+            Console.Write("\nEnter Second Player: ");
+            string? player2 = Console.ReadLine();
+            Console.Clear();
+            if (player1 != null && player2 != null)
+            {
+                return [player1, player2];
+            }
+            else
+            {
+                return ["player 1", "player 2"];
+            }
+        }
+        //Aesthetic Procedures
+        static void loadbar(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Thread.Sleep(100);
+                Console.Write("█");
+            }
+        }
+        
+    }
 }
 
-intro();
+
 
