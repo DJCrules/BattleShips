05bc3353656d2d3f2d14793c650123a3ccce8527
Dylan C <57530020+DJCrules@users.noreply.github.com>
2024-12-12 11:27:10 +0000

    More aesthetics

--- BattleShips/BattleShips/Program.cs
+++ BattleShips/BattleShips/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Buffers;
+﻿using System.Buffers;
+using System.Xml.Serialization;
 
 namespace BattleShips
 {
@@ -7,15 +7,14 @@ namespace BattleShips
     {
         static void Main(string[] args)
         {
-            intro();
+            MenuScreen();
+            Intro();
         }
+
         //Main Procedures
-        static string[] intro()
+        static string[] Intro()
         {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine(" ______  ______  ______ ______ __      ______    \r\n/\\  == \\/\\  __ \\/\\__  _/\\__  _/\\ \\    /\\  ___\\   \r\n\\ \\  __<\\ \\  __ \\/_/\\ \\\\/_/\\ \\\\ \\ \\___\\ \\  __\\   \r\n \\ \\_____\\ \\_\\ \\_\\ \\ \\_\\  \\ \\_\\\\ \\_____\\ \\_____\\ \r\n  \\/_____/\\/_/\\/_/  \\/_/   \\/_/ \\/_____/\\/_____/ \r\n                                                 \r\n       ______  __  __  __  ______ ______         \r\n      /\\  ___\\/\\ \\_\\ \\/\\ \\/\\  == /\\  ___\\        \r\n      \\ \\___  \\ \\  __ \\ \\ \\ \\  _-\\ \\___  \\       \r\n       \\/\\_____\\ \\_\\ \\_\\ \\_\\ \\_\\  \\/\\_____\\      \r\n        \\/_____/\\/_/\\/_/\\/_/\\/_/   \\/_____/  \n\n", Console.ForegroundColor);
-            loadbar(50);
-            Console.ForegroundColor = ConsoleColor.White;
+            title(20);
             Console.Write("\n\nEnter First Player: ", Console.ForegroundColor);
             string? player1 = Console.ReadLine();
             Console.Write("\nEnter Second Player: ");
@@ -30,18 +29,49 @@ namespace BattleShips
                 return ["player 1", "player 2"];
             }
         }
+        static int MenuScreen()
+        {
+            title(50);
+            Console.WriteLine
+                ("==========Menu===========\n\n" +
+                 "1. New Game\n" +
+                 "2. Load Game\n" +
+                 "3. Exit\n\n");
+            Console.Write("Enter choice: ");
+            string choice = Console.ReadLine();
+
+            while (choice == "" | choice == null)
+            {
+                Console.Clear();
+                title(0);
+                Console.WriteLine
+                ("==========Menu===========\n\n" +
+                 "1. New Game\n" +
+                 "2. Load Game\n" +
+                 "3. Exit\n\n");
+                Console.Write("Enter choice: ");
+                choice = Console.ReadLine();
+            }
+            return int.Parse(choice);
+        }
+
         //Aesthetic Procedures
         static void loadbar(int length)
         {
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < 50; i++)
             {
-                Thread.Sleep(100);
+                Thread.Sleep(length);
                 Console.Write("█");
             }
+            Console.WriteLine("\n");
+        }
+        static void title(int n = 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(" ______  ______  ______ ______ __      ______    \r\n/\\  == \\/\\  __ \\/\\__  _/\\__  _/\\ \\    /\\  ___\\   \r\n\\ \\  __<\\ \\  __ \\/_/\\ \\\\/_/\\ \\\\ \\ \\___\\ \\  __\\   \r\n \\ \\_____\\ \\_\\ \\_\\ \\ \\_\\  \\ \\_\\\\ \\_____\\ \\_____\\ \r\n  \\/_____/\\/_/\\/_/  \\/_/   \\/_/ \\/_____/\\/_____/ \r\n                                                 \r\n       ______  __  __  __  ______ ______         \r\n      /\\  ___\\/\\ \\_\\ \\/\\ \\/\\  == /\\  ___\\        \r\n      \\ \\___  \\ \\  __ \\ \\ \\ \\  _-\\ \\___  \\       \r\n       \\/\\_____\\ \\_\\ \\_\\ \\_\\ \\_\\  \\/\\_____\\      \r\n        \\/_____/\\/_/\\/_/\\/_/\\/_/   \\/_____/  \n\n", Console.ForegroundColor);
+            loadbar(n);
+            Console.ForegroundColor = ConsoleColor.White;
         }
         
     }
 }
-
-
-
