952bd678427d252588d5454e8db5039ea0da0424
Dylan C <57530020+DJCrules@users.noreply.github.com>
2024-12-12 10:45:37 +0000

    Names

--- BattleShips/BattleShips/Program.cs
+++ BattleShips/BattleShips/Program.cs
@@ -1,6 +1,11 @@
-﻿static void intro()
+﻿static string[] intro()
 {
-    Console.WriteLine("Welcome to BattleShips");
+    Console.WriteLine("Welcome to BattleShips\n");
+    Console.Write("Enter First Name: ");
+    player1 = Console.ReadLine();
+    Console.Write("\nEnter Second Name: ");
+    player2 = Console.ReadLine();
+    return [player1, player2];
 }
 
 intro();
